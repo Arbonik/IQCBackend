b9c8dae055213c04738691981388b7fcbfd85662
arbonik <zmear@mail.ru>
2023-05-26 22:50:28 +0700

    add core

--- src/main/kotlin/com/mpmep/plugins/core/Game.kt
+++ src/main/kotlin/com/mpmep/plugins/core/Game.kt
@@ -4,10 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 
-class Game{
-    val examples : List<Example> = List(20){
-        generateExample(it)
-    }
+class Game(val examples : List<Example>){
+
     val userAnswers : MutableList<Boolean> = mutableListOf()
     private val _currentExample : MutableStateFlow<Int> = MutableStateFlow(0)
 
@@ -18,11 +16,13 @@ class Game{
     fun checkAnswer(example: Example, answer : Int):Boolean{
         val result = example.result() == answer
         if (result) _currentExample.value ++
+        userAnswers.add(true)
         return result
     }
 
     fun skip(){
         userAnswers.add(false)
+        _currentExample.value++
     }
 }
 
