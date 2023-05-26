84136053dd5196e75ad9ac52158bec803617ed4d
Ivanhai <shatlov.ivan.evgenyevicc@gmail.com>
2023-05-26 22:25:51 +0300

    Game logic

--- src/main/kotlin/com/mpmep/plugins/Routing.kt
+++ src/main/kotlin/com/mpmep/plugins/Routing.kt
@@ -2,14 +2,12 @@ package com.mpmep.plugins
 
 import com.mpmep.classes.GameStatus
 import com.mpmep.classes.Room
-import com.mpmep.plugins.core.Game
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.serialization.encodeToString
@@ -28,7 +26,7 @@ fun Application.configureRouting() {
             post {
                 val room = Room()
                 rooms.add(room)
-                call.respond("${room.id}")
+                call.respond(room.id)
             }
             webSocket("/{id?}") {
                 val id = call.parameters["id"] ?: throw IllegalArgumentException("Room id was excepted")
@@ -38,18 +36,43 @@ fun Application.configureRouting() {
                 room.addPlayer(this)
                 room.roomState.collectLatest { gameStatus ->
                     when (gameStatus){
-                        GameStatus.AWAIT -> {
-                            val status = Json.encodeToString(GameStatus.AWAIT)
+                        is GameStatus.AWAIT -> {
+                            val status = Json.encodeToString(gameStatus)
                             send(Frame.Text(status))
                         }
-                        GameStatus.READY -> {
-                            val status = Json.encodeToString(GameStatus.READY)
+                        is GameStatus.READY -> {
+                            val status = Json.encodeToString(gameStatus)
                             send(Frame.Text(status))
                             room.startGame(this)
                         }
-                        GameStatus.SHUTDOWN -> {
-                            room.players.forEach {
-                                it.close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        is GameStatus.SHUTDOWN -> {
+                            close(CloseReason(CloseReason.Codes.NORMAL, "Room was closed"))
+                        }
+                        is GameStatus.ENEMY_GOT_NEW_EXAMPLE -> {
+                            if (gameStatus.receiver == this) {
+                                val statusString = Json.encodeToString(GameStatus.ENEMY_GOT_NEW_EXAMPLE)
+                                send(Frame.Text(statusString))
+                            }
+                        }
+                        is GameStatus.FALSE -> {
+                            if (gameStatus.receiver == this) {
+                                val statusString = Json.encodeToString(GameStatus.FALSE)
+                                send(Frame.Text(statusString))
+                            }
+                        }
+                        is GameStatus.FINISH -> {
+                            if (gameStatus.receiver == this) {
+                                val statusString = Json.encodeToString(GameStatus.FINISH)
+                                send(Frame.Text(statusString))
+                            }
+                        }
+                        is GameStatus.WIN -> {
+                            val loseStatus = Json.encodeToString(GameStatus.LOSE)
+                            val winStatus = Json.encodeToString(GameStatus.WIN)
+                            if (gameStatus.receiver == this) {
+                                send(Frame.Text(winStatus))
+                            } else {
+                                send(Frame.Text(loseStatus))
                             }
                         }
                         else ->{}
