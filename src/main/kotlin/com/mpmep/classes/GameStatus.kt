d27240873efd50771c6a4105627dfd4c6213dd06
arbonik <zmear@mail.ru>
2023-05-27 11:26:11 +0700

    add WSServerResponse

--- src/main/kotlin/com/mpmep/classes/GameStatus.kt
+++ src/main/kotlin/com/mpmep/classes/GameStatus.kt
@@ -4,13 +4,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class GameStatus {
-    READY,
-    FALSE,
-    GOT_NEW_EXAMPLE,
-    FINISH,
-    WIN,
-    LOSE,
-    SHUTDOWN,
-    AWAIT,
-    EMPTY
+    READY, // оба игрока подключились
+    FALSE, //  игрок ошибся - не обрабатывается
+    GOT_NEW_EXAMPLE, // противник решил очередной пример
+    FINISH, // примеры кончились, игрок ждет противника
+    WIN, // победитель
+    LOSE, // проигравший
+    SHUTDOWN, // игра закончилась
+    AWAIT, // игрок ожидает противника
+    EMPTY // пришел новый пример
 }
